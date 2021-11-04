26e85e5bc7098db01a697ac5a4445517049e78ef
Markus Kaihola <makeri89@hotmail.com>
2021-11-04 11:56:00 +0200

    test coverage to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,43 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_negatiivisen_maaran_lisaaminen_ei_onnistu(self):
+        alkuperainen_tila = self.varasto.paljonko_mahtuu()
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), alkuperainen_tila)
+        
+    def test_liian_suuren_maaran_lisaamine_ei_ylita_tilavuutta(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+        
+    def test_negatiivisen_maaran_ottaminen_ei_onnistu(self):
+        self.varasto.lisaa_varastoon(5)
+        alkuperainen_saldo = self.varasto.saldo
+        self.varasto.ota_varastosta(-5)
+        self.assertAlmostEqual(self.varasto.saldo, alkuperainen_saldo)
+        
+    def test_liian_suuren_maaran_ottaminen_ei_onnistu(self):
+        self.varasto.lisaa_varastoon(5)
+        otto = self.varasto.ota_varastosta(8)
+        self.assertAlmostEqual(otto, 5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        
+    def test_tuloste_on_oikeanlainen(self):
+        haluttu_tuloste = 'saldo = 4, vielä tilaa 6'
+        self.varasto.lisaa_varastoon(4)
+        self.assertEqual(self.varasto.__str__(), haluttu_tuloste)
+            
+    def test_negatiivinen_tilavuus_nollaantuu(self):
+        uusi_varasto = Varasto(-10)
+        self.assertAlmostEqual(uusi_varasto.tilavuus, 0)
+        
+    def test_negatiivinen_alku_saldo_nollaantuu(self):
+        uusi_varasto = Varasto(10,-10)
+        self.assertAlmostEqual(uusi_varasto.saldo, 0)
+        
+    def test_liian_suuri_alku_saldo(self):
+        uusi_varasto = Varasto(5,10)
+        self.assertAlmostEqual(uusi_varasto.saldo, uusi_varasto.tilavuus)
+        
+    
